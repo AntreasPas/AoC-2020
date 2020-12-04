0e90be631f69bcc6836ba0e448f9c6e02af2b8ab
Antreas Paschalis <vitas13@gmail.com>
2020-12-04 10:52:20 +0000

    Add day 4

---
+++ src/day4/Main.java
@@ -0,0 +1,38 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static List<Passport> readPassports()
+            throws IOException {
+        List<Passport> passports = new ArrayList<>();
+        try (BufferedReader br
+                     = new BufferedReader(new FileReader("./src/day4/input.txt"))) {
+            String line;
+            Passport passport = new Passport();
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    passports.add(passport);
+                    passport = new Passport();
+                } else {
+                    passport.addFields(line.split(" "));
+                }
+            }
+            passports.add(passport);
+        }
+        return passports;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Passport> passports = readPassports();
+        System.out.println("----- DAY 4 - PART 1 -----");
+        System.out.printf("Valid passports: %d%n", passports.stream().filter(Passport::isValidPart1).count());
+        System.out.println("----- DAY 4 - PART 2 -----");
+        System.out.printf("Valid passports: %d%n", passports.stream().filter(Passport::isValidPart2).count());
+    }
+}
---
+++ src/day4/Passport.java
@@ -0,0 +1,75 @@
+package day4;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Passport {
+    private static String[] REQUIRED_FIELDS = new String[]{
+            "byr",
+            "iyr",
+            "eyr",
+            "hgt",
+            "hcl",
+            "ecl",
+            "pid",
+    };
+    private static final Set<String> VALID_EYE_COLOURS = Collections.unmodifiableSet(
+            Set.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+    );
+    private final Map<String, String> map = new HashMap<>();
+
+
+    public void addFields(String[] fields) {
+        Arrays.stream(fields).map(field -> field.split(":")).forEach(field -> map.put(field[0], field[1]));
+    }
+
+    public boolean isValidPart1() {
+        return REQUIRED_FIELDS.length ==
+                Arrays.stream(REQUIRED_FIELDS)
+                        .filter(map::containsKey)
+                        .count();
+    }
+
+    public boolean isValidPart2() {
+        return isValidPart1()
+                && validateNumberInRange(map.get("byr"), 1920, 2002)
+                && validateNumberInRange(map.get("iyr"), 2010, 2020)
+                && validateNumberInRange(map.get("eyr"), 2020, 2030)
+                && validateHeight()
+                && validateRegex(map.get("hcl"), "^#([0-9]|[a-f]){6}$")
+                && validateEyeColour()
+                && validateRegex(map.get("pid"), "^\\d{9}$");
+
+    }
+
+    private boolean validateEyeColour() {
+        return VALID_EYE_COLOURS.contains(map.get("ecl"));
+    }
+
+    private boolean validateRegex(String field, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(field).matches();
+    }
+
+    private boolean validateHeight() {
+        String height = map.get("hgt");
+        if (height.endsWith("cm")) {
+            height = height.replace("cm", "");
+            return validateNumberInRange(height, 150, 193);
+        } else if (height.endsWith("in")) {
+            height = height.replace("in", "");
+            return validateNumberInRange(height, 59, 76);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean validateNumberInRange(String field, int min, int max) {
+        try {
+            int value = Integer.parseInt(field);
+            return min <= value && value <= max;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
