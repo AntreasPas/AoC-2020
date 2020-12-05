be9e40162b029ba463bb42b568963836866df264
Antreas Paschalis <vitas13@gmail.com>
2020-12-05 17:24:34 +0000

    Optimise day 5 a bit

--- src/day5/Main.java
+++ src/day5/Main.java
@@ -3,14 +3,12 @@ package day5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
     private static int findMySeat(List<Integer> seatIds) {
-        Collections.sort(seatIds);
         for (int i = 0; i < seatIds.size() - 1; i++) {
             if (seatIds.get(i) + 1 != seatIds.get(i + 1))
                 return seatIds.get(i) + 1;
@@ -23,11 +21,10 @@ public class Main {
                 .lines(Path.of("./src/day5/input.txt"))
                 .map(Seat::new)
                 .map(Seat::getSeatId)
+                .sorted()
                 .collect(Collectors.toList());
         System.out.println("----- DAY 5 - PART 1 -----");
-        System.out.printf("Highest seat ID: %d%n", seatIds.stream()
-                .max(Integer::compareTo)
-                .orElseThrow());
+        System.out.printf("Highest seat ID: %d%n", seatIds.get(seatIds.size() - 1));
         System.out.println("----- DAY 5 - PART 2 -----");
         System.out.printf("My seat ID: %d%n", findMySeat(seatIds));
     }
